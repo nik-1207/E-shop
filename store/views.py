5202af61a2248d64e6fb3fbd1c322e5c38724211
nik-1207 <nikhil.mohan_cs18@gla.ac.in>
2021-01-30 23:48:37 +0530

    password hashing implemented
    Signed-off-by: nik-1207 <nikhil.mohan_cs18@gla.ac.in>

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.shortcuts import render
 
 from .models.Category import Category
@@ -18,14 +19,15 @@ def signup(response):
     if response.method == 'GET':
         return render(response, 'signup.html')
     user_dict = response.POST
-
     new_customer = Customer(first_name=user_dict.get('fname'),
                             last_name=user_dict.get('lname'),
                             phone=user_dict.get('phone'),
                             email=user_dict.get('email'),
-                            password=user_dict.get('pswd'))
+                            password=make_password(user_dict.get('pswd')))
     try:
-        print(Customer.objects.get(email=new_customer.email))
+        # check preexisting email
+        Customer.objects.get(email=new_customer.email)
+        # if email pre exist error is displayed on form
         return render(response, 'signup.html', {'error': 'Email already exist'})
         # email already exist
     except:
