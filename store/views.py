72c4288b6c13909a714ef6e53d010c5981c27d77
nik-1207 <nikhil.mohan_cs18@gla.ac.in>
2021-01-30 22:49:44 +0530

    forms complete with unique email for signup
    Signed-off-by: nik-1207 <nikhil.mohan_cs18@gla.ac.in>

--- store/views.py
+++ store/views.py
@@ -18,11 +18,16 @@ def signup(response):
     if response.method == 'GET':
         return render(response, 'signup.html')
     user_dict = response.POST
-    print(response.POST)
-    new_customer = Customer.objects.create(first_name=user_dict.get('fname'),
-                                           last_name=user_dict.get('lname'),
-                                           phone=user_dict.get('phone'),
-                                           email=user_dict.get('email'),
-                                           password=user_dict.get('pswd'))
-    new_customer.save()
-    return index(response)
+
+    new_customer = Customer(first_name=user_dict.get('fname'),
+                            last_name=user_dict.get('lname'),
+                            phone=user_dict.get('phone'),
+                            email=user_dict.get('email'),
+                            password=user_dict.get('pswd'))
+    try:
+        print(Customer.objects.get(email=new_customer.email))
+        return render(response, 'signup.html', {'error': 'Email already exist'})
+        # email already exist
+    except:
+        new_customer.save()
+        return index(response)
